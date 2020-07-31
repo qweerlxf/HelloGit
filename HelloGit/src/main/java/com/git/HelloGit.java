e9294e9dd791a63ec236543e9b0cdea346bfce76
qweerlxf <1434442570@qq.com>
2020-07-31 22:55:45 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author luoxiaofang
+ * @create 2020-07-31 22:42
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
